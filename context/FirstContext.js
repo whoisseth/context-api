1cb7923d88f22d92f203e9253052041f4f25e317
utkarsh <55540613+whoisseth@users.noreply.github.com>
2022-03-31 09:15:17 +0530

    use getStaticProps  in home page

--- context/FirstContext.js
+++ context/FirstContext.js
@@ -1,28 +1,21 @@
 import { createContext, useState, useEffect } from 'react'
 export const Context = createContext()
-const CryptoData = 'https://api2.binance.com/api/v3/ticker/24hr'
-
-function FirstProvider({ children, data }) {
+// import useLocalStorage from 'react-use-localstorage';
+function FirstProvider({ children }) {
   const [count, setCount] = useState(0)
   // timer
-  const [time, setTime] = useState(0)
+  const [ time, setTime ] = useState(0)
+  // const [ item, setItem ] = useLocalStorage('time', time);
+  
   useEffect(() => {
     setTimeout(() => setTime(time + 1), 1000)
+    // setItem(
   })
-  console.log(data)
   return (
-    <Context.Provider value={{ count, setCount, data, time }}>
+    <Context.Provider value={{ count, setCount,  time }}>
       {children}
     </Context.Provider>
   )
 }
 export default FirstProvider
 
-export async function getStaticProps() {
-  const data = await fetch(CryptoData)
-  // const data = await res.json()
-  console.log(data)
-  // console.log(res)
-
-  return { props: { data } }
-}
